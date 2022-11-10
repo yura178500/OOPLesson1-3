2acd811ac3b2526e9fa4f3804336546dc962e784
yura178500 <yura178500@yandex.ru>
2022-11-11 00:50:47 +0300

    3

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,57 @@
 import transport.Car;
 
+import java.util.Date;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
-        Car ladaGranta = new Car();
-        ladaGranta.getBrand() = "Лада";
-        ladaGranta.getModel() = "Гранта";
-        ladaGranta.color = "желтого";
-        ladaGranta.engineVolume = (int) 1.7;
-        ladaGranta.getProductionCountry() = "Россия";
-        ladaGranta.getProductionYear() = 2015;
+        Car ladaGranta = new Car("Лада", "Гранта", (int) 1.7, "желтого", 2015, "Россия", "МКПП", "универсал", "х000хх000", 5, "summerTires");
+        Car audiA8 = new Car("Audi ", "A8", (int) 3.0, "черный", 2020, "Германия", "АКПП", "универсал", "х000хх000", 5, "summerTires");
+        Car bmwZ8 = new Car("BMW ", "Z8", (int) 3.0, "черный", 2021, "Германия", "МКПП", "универсал", "х000хх000", 5, "winterTires");
+        Car kia = new Car(" Kia", "Sportage", (int) 2.4, "красный", 2018, "Южной Корее", "АКПП", "универсал", "х000хх000", 5, "summerTires");
+        Car hyunda = new Car(" Hyundai", "Avante", (int) 1.6, " оранжевый", 2016, "Южной Корее", "МКПП", "универсал", "х000хх000", 5, "summerTires");
 
-        Car audiA8 = new Car();
-        audiA8.getBrand() = "Audi ";
-        audiA8.getModel() = "A8";
-        audiA8.color = "черный";
-        audiA8.engineVolume = (int) 3.0;
-        audiA8.getProductionCountry() = "Германия";
-        audiA8.getProductionYear() = 2020;
-
-        Car bmwZ8 = new Car();
-        bmwZ8.getBrand() = "BMW ";
-        bmwZ8.getModel() = "Z8";
-        bmwZ8.color = "черный";
-        bmwZ8.engineVolume = (int) 3.0;
-        bmwZ8.getProductionCountry() = "Германия";
-        bmwZ8.getProductionYear() = 2021;
-
-        Car kia = new Car();
-        kia.getBrand() = " Kia";
-        kia.getModel() = "Sportage";
-        kia.color = "красный";
-        kia.engineVolume = (int) 2.4;
-        kia.getProductionCountry() = "Южной Корее";
-        kia.getProductionYear() = 2018;
-
-        Car hyunda = new Car();
-        hyunda.getBrand() = " Hyundai";
-        hyunda.getModel() = "Avante";
-        hyunda.color = " оранжевый";
-        hyunda.engineVolume = (int) 1.6;
-        hyunda.getProductionCountry() = "Южной Корее";
-        hyunda.getProductionYear() = 2016;
-
-
-        System.out.println("- марка " + kia.getBrand() + " - модель " + kia.getModel() + "- объем двигателя в литрах" + kia.engineVolume + "- цвет кузова" + kia.color +
+        System.out.println("- марка " + kia.getBrand() + " - модель " + kia.getModel() + "- объем двигателя в литрах" + kia.engineVolume + "- цвет кузова " + kia.color +
                 "- год производства" + kia.getProductionYear() + " - страна сборки" + kia.getProductionCountry());
-        System.out.println("- марка " + hyunda.getBrand() + " - модель " + hyunda.getModel() + "- объем двигателя в литрах" + hyunda.engineVolume + "- цвет кузова" + hyunda.color +
-                "- год производства" + hyunda.getProductionYear() +  " - страна сборки" + hyunda.getProductionCountry());
-        System.out.println("- марка " + ladaGranta.getBrand() + " - модель " + ladaGranta.getModel() + "- объем двигателя в литрах" + ladaGranta.engineVolume + "- цвет кузова" + ladaGranta.color +
+        System.out.println("- марка " + hyunda.getBrand() + " - модель " + hyunda.getModel() + "- объем двигателя в литрах" + hyunda.engineVolume + "- цвет кузова " + hyunda.color +
+                "- год производства" + hyunda.getProductionYear() + " - страна сборки" + hyunda.getProductionCountry());
+        System.out.println("- марка " + ladaGranta.getBrand() + " - модель " + ladaGranta.getModel() + "- объем двигателя в литрах" + ladaGranta.engineVolume + "- цвет кузова " + ladaGranta.color +
                 "- год производства" + ladaGranta.getProductionCountry() + " - страна сборки" + ladaGranta.getProductionCountry());
         System.out.println("- марка " + bmwZ8.getBrand() + " - модель " + bmwZ8.getModel() + "- объем двигателя в литрах" + bmwZ8.engineVolume + "- цвет кузова" + bmwZ8.color +
                 "- год производства" + bmwZ8.getProductionCountry() + " - страна сборки" + bmwZ8.getProductionYear());
         System.out.println("- марка " + audiA8.getBrand() + " - модель " + audiA8.getModel() + "- объем двигателя в литрах" + audiA8.engineVolume + "- цвет кузова" + audiA8.color +
-                "- год производства" + audiA8.getProductionCountry() +  " - страна сборки" + audiA8.getProductionYear());
+                "- год производства" + audiA8.getProductionCountry() + " - страна сборки" + audiA8.getProductionYear());
 
         System.out.println("===============================================================================================================================================================");
+        System.out.println(ladaGranta);
+        System.out.println(audiA8);
+        System.out.println(bmwZ8);
+        System.out.println(kia);
+        System.out.println(hyunda);
 
-        boolean winterTires = true;
-        boolean summerTires = true;
-        System.out.println(winterTires == summerTires);
-        System.out.println(true == winterTires);
-        System.out.println((boolean) true == winterTires);
+
+        ladaGranta.setSummerTyre();
+        System.out.println(ladaGranta.isSummerTyre);
+        System.out.println(audiA8.isSummerTyre);
+        System.out.println(bmwZ8.isSummerTyre);
+        System.out.println(kia.isSummerTyre);
+        System.out.println(hyunda.isSummerTyre);
+
+        ladaGranta.registrationNumber("С065МК 78RUS");
+        Car.CarStateNumberValidator stateNumber = new Car.CarStateNumberValidator();
+        String st = ladaGranta.getRegistrationNumber();
+        System.out.println(stateNumber.isValid(st));
+
+        Date date1 = new Date(2022, 1, 1);
+        Date date2 = new Date(2023, 1, 1);
+        int result = date1.compareTo(date2);
+        if( result ==0)
+            System.out.println("Both dates are equal");
+        else if(result < 0)
+            System.out.println("Date1 is before Date2");
+        else
+            System.out.println("Date1 is after Date2");
 
 
     }
-}
+       }
--- src/transport/Car.java
+++ src/transport/Car.java
@@ -1,6 +1,12 @@
 package transport;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Car {
+    public String isSummerTyre;
     private String brand;
     private String model;
     public int engineVolume;
@@ -10,32 +16,163 @@ public class Car {
 
     public String transmission;
     private String bodyType;
-    public int registrationNumber;
+    public String registrationNumber;
     private int numberOfSeats;
 
-    public String winterTires;
-    public String summerTires;
+    public boolean summerTyre;
 
-    public String getWinterTires() {
-        this.winterTires = winterTires;
-        return winterTires;
-    }
-
-    public void setWinterTires(String winterTires) {
-        this.winterTires = winterTires;
-    }
-
-    public String getSummerTires() {
-        this.summerTires = summerTires;
-        return summerTires;
+    public void setSummerTyre(boolean summerTyre) {
+        this.summerTyre = summerTyre;
     }
 
     public void setSummerTires(String summerTires) {
         this.summerTires = summerTires;
     }
 
-    public String getBrand() {
+    public String winterTires = "Зимние шины";
+    public String summerTires = "Летние шины";
+
+    private class Key {
+        private String remoteEngineStart;
+        private String keylessAccess;
+
+        public Key(String remoteEngineStart, String keylessAccess) {
+            this.remoteEngineStart = remoteEngineStart;
+            this.keylessAccess = keylessAccess;
+        }
+
+        public String getRemoteEngineStart() {
+
+            if (remoteEngineStart == null || remoteEngineStart.isEmpty()) {
+                this.remoteEngineStart = remoteEngineStart;
+
+            } else {
+                this.remoteEngineStart = "Некорректное значение";
+            }
+            return remoteEngineStart;
+        }
+
+        public void setRemoteEngineStart(String remoteEngineStart) {
+            this.remoteEngineStart = remoteEngineStart;
+        }
+
+        public String getKeylessAccess() {
+
+            if (keylessAccess == null || keylessAccess.isEmpty()) {
+                this.keylessAccess = keylessAccess;
+
+            } else {
+                this.keylessAccess = "Некорректное значение";
+            }
+            return keylessAccess;
+        }
+
+        public void setKeylessAccess(String keylessAccess) {
+            this.keylessAccess = keylessAccess;
+        }
+
+        private class Insurance {
+            private int validityPeriod;
+            private int cost;
+            private int number;
+            public int date;
+
+            public Insurance(int validityPeriod, int cost, int number) {
+                this.validityPeriod = validityPeriod;
+                this.cost = cost;
+                this.number = number;
+
+            }
+
+            public int getValidityPeriod() {
+
+                if (validityPeriod <= 0) {
+                    this.validityPeriod = validityPeriod;
+
+                } else {
+                    this.validityPeriod = Math.abs(validityPeriod);
+                }
+                return validityPeriod;
+
+            }
+
+            public void setValidityPeriod(int validityPeriod) {
+                this.validityPeriod = validityPeriod;
+            }
+
+            public int getCost() {
+
+                if (cost <= 0) {
+                    this.cost = cost;
+
+                } else {
+                    this.cost = Math.abs(cost);
+                }
+                return cost;
+            }
+
+            public void setCost(int cost) {
+                this.cost = cost;
+            }
+
+            public int getNumber() {
+
+                if (number <= 0) {
+                    this.number = number;
+
+                } else {
+                    this.number = Math.abs(number);
+                }
+                return number;
+            }
+
+            public void setNumber(int number) {
+                this.number = number;
+            }
+
+        }
+    }
+
+
+    public Car(String brand, String model, int engineVolume, String color, int productionYear, String productionCountry, String transmission, String bodyType, String registrationNumber, int numberOfSeats, String isSummerTyre) {
         this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.productionYear = productionYear;
+        this.productionCountry = productionCountry;
+        this.transmission = transmission;
+        this.bodyType = bodyType;
+        this.registrationNumber = String.valueOf(registrationNumber);
+        this.numberOfSeats = numberOfSeats;
+        this.isSummerTyre = String.valueOf(isSummerTyre());
+    }
+
+    public String getWinterTires() {
+        this.winterTires = winterTires;
+        return winterTires;
+    }
+
+
+    public void setWinterTires(String winterTires) {
+        this.winterTires = winterTires;
+    }
+
+
+    public void setSummerTyre() {
+        this.summerTyre = summerTyre;
+    }
+
+    public void setSummerTires() {
+        this.summerTires = summerTires;
+    }
+
+    public String getBrand() {
+
+        if (brand == null || brand.isEmpty()) {
+            this.brand = brand;
+        } else {
+        }
         return brand;
     }
 
@@ -117,18 +254,18 @@ public class Car {
         this.bodyType = bodyType;
     }
 
-    public int getRegistrationNumber() {
+    public String getRegistrationNumber() {
 
-        if (registrationNumber < 0) {
+        if (registrationNumber == null || registrationNumber.isEmpty()) {
             this.registrationNumber = registrationNumber;
         } else {
-            this.registrationNumber = Math.abs(registrationNumber);
+            this.registrationNumber = "Некорректное значение";
         }
         return registrationNumber;
     }
 
-    public void setRegistrationNumber(int registrationNumber) {
-        this.registrationNumber = registrationNumber;
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = String.valueOf(registrationNumber);
     }
 
     public int getNumberOfSeats() {
@@ -146,5 +283,68 @@ public class Car {
         this.numberOfSeats = numberOfSeats;
     }
 
+    public String toString() {
+        return "Car { " +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", engineVolume=" + engineVolume +
+                ", color='" + color + '\'' +
+                ", productionYear=" + productionYear +
+                ", productionCountry='" + productionCountry + '\'' +
+                ", transmission='" + transmission + '\'' +
+                ", bodyType='" + bodyType + '\'' +
+                ", registrationNumber='" + registrationNumber + '\'' +
+                ", numberOfSeats=" + numberOfSeats +
+                ", summerTyre='" + isSummerTyre + '\'' + '}';
+    }
+
+    public boolean getSummerTire() {
+
+        return getSummerTire();
+    }
+
+    public boolean isSummerTyre() {
+        boolean summerTyre = Boolean.parseBoolean(winterTires);
+        boolean statement2 = Boolean.parseBoolean(summerTires);
+
+        if (summerTyre) {
+            System.out.println("winterTires");
+        } else if (statement2) {
+            System.out.println("summerTires");
+        } else {
+            System.out.println("Оба высказывания ложные.");
+        }
+
+        return summerTyre;
+    }
+
+    public void registrationNumber(String s) {
+    }
+
+    public interface Validator<T> {
+
+        boolean isValid(T t);
+    }
+
+    public static class CarStateNumberValidator implements Validator<String> {
+        public boolean isValid(String registrationNumber) {
+            Pattern registrationNumberPattern = Pattern.compile(
+                    "([А-Яа-я])" +
+                            "([0-9]){3}" +
+                            "([А-Яа-я]){2}" +
+                            "[\\s]" +
+                            "([0-9]){2}|([0-9]){3}" +
+                            "[RUS]"
+            );
+            Matcher registrationNumberMatcher = registrationNumberPattern.matcher(registrationNumber);
+            if (registrationNumberMatcher.matches()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    }
 }
+
 
